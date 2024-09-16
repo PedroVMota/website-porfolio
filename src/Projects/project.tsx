665f3ce8753fed3b7cd835a8c8899fd99fac31e7
PedroVMota <pvital-m@student.42lisboa.com>
2024-09-16 17:41:46 +0100

    Done

--- src/Projects/project.tsx
+++
@@ -1,7 +0,0 @@
-export default function Project() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-4xl font-bold text-blue-500">Project</h1>
-        </div>
-    );
-}
